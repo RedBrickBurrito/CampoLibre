67740a832566748dc9711f59e2c9656676490eb4
redbrickburrito <a01568010@gmail.com>
2023-06-26 20:57:19 -0600

    CAM-4 Added NavBar and Shopping Cart component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,27 @@
 import { useSession, signOut } from "next-auth/react"
+import Image from "next/image"
+import Navbar from "@ui/Navbar/Navbar"
+import ShoppingCart from "@ui/Sideovers/ShoppingCart"
+import { useState } from 'react';
+
 
 export default function Home() {
   const { data: session } = useSession()
+  const [cartOpen, setCartOpen] = useState(false);
+
+  const handleCartClose = () => {
+    setCartOpen(false);
+  };  
 
   return (
     <>
-      <h1>Home</h1>
-      <button onClick={() => signOut()}>Cerrar Sesión</button>
+      <Navbar />
+      <div>
+        <h1>Home</h1>
+        <button onClick={() => signOut()}>Cerrar Sesión</button>
+        <Image onClick={() => setCartOpen(true)} src="/tomate_saladette.jpeg" alt="tomate saladette" width="120" height="120"/>
+        {cartOpen && <ShoppingCart onClose={handleCartClose}/>}
+      </div>
     </>
   )
 }
